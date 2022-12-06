b974919eb78325cc5056510439e4f6afe59fd6b0
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-12-06 10:53:34 +0530

    Initial commit

---
+++ src/main/java/com/example/CourseApp_Backend/controller/courses.java
@@ -0,0 +1,12 @@
+package com.example.CourseApp_Backend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class courses {
+
+    @Override
+    public String toString() {
+        return ""
+    }
+}
