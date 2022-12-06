849c13f9d0a81dc83836b5012ce3f96b58e512f4
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-12-06 11:13:33 +0530

    add page dded

--- src/main/java/com/example/CourseApp_Backend/controller/courses.java
+++ src/main/java/com/example/CourseApp_Backend/controller/courses.java
@@ -1,6 +1,9 @@
 package com.example.CourseApp_Backend.controller;
 
+import com.example.CourseApp_Backend.model.Course;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +12,10 @@ public class courses {
     @GetMapping("/")
     public String welcome() {
         return "welcome page";
+    }
+    @PostMapping(path = "/add",consumes = "application/json",produces = "application/json")
+    public String add(@RequestBody Course c) {
+        System.out.println(c.toString());
+        return "added successfully";
     }
 }
