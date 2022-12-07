2874886ccef897f1496874bf86f6a8f518ae0f2f
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-12-07 17:17:44 +0530

    search

--- src/main/java/com/example/CourseApp_Backend/controller/courseController.java
+++ src/main/java/com/example/CourseApp_Backend/controller/courseController.java
@@ -4,6 +4,7 @@ import com.example.CourseApp_Backend.Resp;
 import com.example.CourseApp_Backend.dao.CourseDao;
 import com.example.CourseApp_Backend.model.Course;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -32,19 +33,28 @@ public class courseController {
     }
 @CrossOrigin(origins = "*")
     @DeleteMapping("/delete")
-    public Map<String, String>delete(@RequestBody Resp resp) {
+    public Map<String, String>delete(@RequestBody Resp  resp) {
         List<Course> courses = (List<Course>) dao.findAll();
         HashMap<String, String> map = new HashMap<>();
-
+    System.out.println(courses);
         for(int i = 0; i < courses.size(); i++) {
 
             Course c = courses.get(i);
+
             map.put("inside", c.getTitle());
+            System.out.println("title"+c.getTitle());
         if (c.getTitle().equals(resp.getName())) {
-            dao.delete(c);
+            dao.delete( c);
             map.put("status","success");
         }
 }
         return map;
+    }
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/search",consumes = "application/json",produces = "application/json")
+    public List<Course> search(@RequestBody Course c) {
+        String courseTitle= String.valueOf(c.getTitle());
+        System.out.println(courseTitle);
+        return (List<Course>) dao.searchEmployees(c.getTitle());
     }
 }
